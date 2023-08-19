5951b4ec42ff1d1d6457cb366beba68b06a25683
shabestari <niloufar2012shabestari@gmail.com>
2023-08-19 10:17:21 +0330

    created app theme requirements

---
+++ app/src/main/java/com/ethereum/ethereumwallet/ui/theme/Shape.kt
@@ -0,0 +1,12 @@
+package com.ethereum.ethereumwallet.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+// define the shapes used in app
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
