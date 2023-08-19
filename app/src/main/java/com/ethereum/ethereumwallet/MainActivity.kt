97f80cbe6fc223c12c03199d3d2125073caef233
shabestari <niloufar2012shabestari@gmail.com>
2023-08-19 10:17:21 +0330

    created app theme requirements

--- app/src/main/java/com/ethereum/ethereumwallet/MainActivity.kt
+++ app/src/main/java/com/ethereum/ethereumwallet/MainActivity.kt
@@ -2,10 +2,29 @@ package com.ethereum.ethereumwallet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import com.ethereum.ethereumwallet.ui.theme.AppTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+
+        setContent {
+            AppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+//                    walletAddressView()
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/ethereum/ethereumwallet/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.ethereum.ethereumwallet.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+//define the colors used in app
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+val myLightGray = Color(0xFF9E9E9E)
